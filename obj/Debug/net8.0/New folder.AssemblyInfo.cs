58230354c236db1f80d6ace0e99280e48dc9b79e
akatsuki0354 <francogregorio2004@gmail.com>
2024-11-04 00:09:08 +0800

    initialize

--- obj/Debug/net8.0/New folder.AssemblyInfo.cs
+++ obj/Debug/net8.0/New folder.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("New folder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+363aa689b61a1575df09ac1ac558be1d84b5e7f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6da26b5544f4150981c9e3c33ddbccd86e36dd9")]
 [assembly: System.Reflection.AssemblyProductAttribute("New folder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("New folder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
